8b9408579e5ca7a6df14884d9ed44e0b69a670c4
majiyami <andrew46100305@gmail.com>
2022-09-04 04:19:10 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const app = express();
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('opne', () => {
+  console.log('mobgodb 111111111111')
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
