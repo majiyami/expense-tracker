586088bc66fd3a4e3be97e9911ff54e30b370819
majiyami <andrew46100305@gmail.com>
2022-09-08 02:02:39 +0800

    plus new name

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const app = express();
@@ -13,7 +16,7 @@ const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
- 
+
 db.once('open', () => {
   console.log('mongodb connected!')
 })
@@ -21,11 +24,24 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ exrended: true }))
+
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
