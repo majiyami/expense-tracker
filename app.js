3c1729569b9fb29a7f7a5b515dfb98badc8f2cda
majiyami <andrew46100305@gmail.com>
2022-09-11 00:19:49 +0800

    mongooseJS

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 
-const Todo = require('./models/todo')
+require('./config/mongoose')
 
 
 const app = express();
 
 const port = 3000
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
