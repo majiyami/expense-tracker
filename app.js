de3ea76a701c8f864ed1c2b155dd3e8c828c89b0
majiyami <andrew46100305@gmail.com>
2022-11-05 01:22:45 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
@@ -14,6 +15,11 @@ const port = 3000
 app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: "ThisIsMySecret",
+  resave: false,
+  saveUninitialized: true,
+}))
 app.use(bodyParser.urlencoded({ exrended: true }))
 app.use(methodOverride('_method'))
 
