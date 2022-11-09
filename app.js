e331576b75cab2d902ab628cbf02711a51803e03
majiyami <andrew46100305@gmail.com>
2022-11-09 19:05:28 +0800

    feat: add dotenv & facebook login

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const bodyParser = require('body-parser')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
@@ -13,13 +16,13 @@ require('./config/mongoose')
 
 const app = express();
 
-const port = 3000
+const port = process.env.PORT
 
 app.engine('hbs', exphbs({ default: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: "ThisIsMySecret",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
 }))
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
+router.use('/auth', auth)
 router.use('/users', users)
 router.use('/', authenticator, home)
 router.use('/', authenticator, todos)
+
 
 
 module.exports = router
